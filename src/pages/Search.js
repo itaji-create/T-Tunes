6b8719db84207156d0a9a0c8016a6e048817d4b3
Itaji Carvalho <itaji.1999@gmail.com>
2021-11-13 18:44:54 -0300

    desafio 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,36 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange({ target }) {
+    const { value } = target;
+    this.setState({ search: value });
+  }
+
   render() {
+    const { search } = this.state;
+    const minLength = 2;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <input
+          data-testid="search-artist-input"
+          onChange={ this.onInputChange }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ search.length < minLength }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
